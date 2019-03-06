b5e6115006b74319f2895bb5b4d9438ed2405f94
dh.praneeth@gmail.com <dh.praneeth@gmail.com>
2019-03-06 22:11:05 +0530

    added

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
-import { starComponent } from './shared/star.component';
+import { StarComponent } from './shared/star.component';
 
 
 
@@ -14,11 +15,12 @@ import { starComponent } from './shared/star.component';
     AppComponent,
     ProductListComponent,
     ConvertToSpacesPipe,
-    starComponent
+    StarComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    HttpClientModule
     
   ],
   bootstrap: [AppComponent]
